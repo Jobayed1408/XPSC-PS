c744e972244dfe869ceb1ecad427b375bafaaec4
Jobayed1408 <mdjobayedislam4@gmail.com>
2023-06-28 10:49:14 +0600

    day 5

--- week 4/day 5/1 A. Two Elevators.cpp
+++ week 4/day 5/1 A. Two Elevators.cpp
@@ -6,47 +6,13 @@ int main()
 	int t; cin>>t;
 	while(t--)
 	{
-		string s; cin>>s;
-		int n = s.size();
+		int a,b,c; cin>>a>>b>>c;
 		
-		vector< pair<char, int> > order;
+//		if(b == 1) b+=2;
 		
-		char mn = min(s[0], s[n-1]);
-		char mx = max(s[0],s[n-1]);
+		if(abs(a-1) < abs(b-c)+(c-1) ) cout<<1<<endl;
+		else if( abs(a-1) >abs(b-c)+(c-1) ) cout<<2<<endl;
+		else cout<<3<<endl;
 		
-		for(int i =0 ; i<n; i++)
-		{
-			if(s[i] >= mn && s[i] <= mx)
-				order.push_back( {s[i] , i} );
-		}
-		
-		sort(order.begin() , order.end() , [&] (pair<char , int>a , pair<char , int> b){
-			return a.second < b.second;
-		});
-		
-		if(order.size() > 2){
-			sort(order.begin()+1, order.begin()+order.size()-1 , [&](pair<char , int>a, pair<char, int>b){
-				if(s[0] > s[n-1]) return a.first > b.first;
-				return a.first < b.first;
-			});
-		}
-		
-		vector<int> seq;
-		int moves = 0;
-		seq.push_back(1);
-		
-		for(int i=1; i<order.size(); i++)
-		{
-			moves+=abs( order[i-1].first - order[i].first );
-			seq.push_back(order[i].second+1);
-		}
-		
-//		cout << abs(s[0] - s[s.size()-1]);
-		cout<<moves<<" "<<seq.size()<<endl;
-		for(auto i : seq)
-		{
-			cout<< i <<" ";
-		}
-		cout<<endl;
 	}
 }
