d18bc455326ddc3ad8163854bc63500b052b149f
Jobayed1408 <mdjobayedislam4@gmail.com>
2023-07-08 23:55:31 +0600

    day 1

---
+++ week 5/day 1/A. Polycarp and Sums of Subsequences.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		int n = 7;
+		int arr[n];
+		for(int i=0; i<n; i++)
+		{
+			cin>>arr[i];
+		}
+		cout<<arr[0]<<" "<<arr[1]<<" "<<arr[6]-arr[0]-arr[1]<<endl;
+	}
+}
